7045e02a54cbbe6d69714485b2aa20742e603060
Nick Buonincontri <nbuonin@gmail.com>
2016-09-14 15:00:23 -0400

    stub for js to fetch xml news feed

--- static/js/custom.js
+++ static/js/custom.js
@@ -11,8 +11,35 @@ meth.bootstrap_drop_menu = function(){
 
 }
 
-jQuery(document).ready(function(){
+meth.build_news_feed = function() {
+  // var xmlFeed;
+
+  // var feedRequest = new XMLHttpRequest();
+  // feedRequest.open('GET', 'http://ctl.columbia.edu/tag/mediathread/feed/');
+  // // feedRequest.setRequestHeader("Authorization", "Negotiate");
+  // feedRequest.send();
+  // console.log(feedRequest.getAllResponseHeaders());
+
+  // this should work, but doesn't:
+  // var feedRequest = $.ajax({
+  //   method: 'GET',
+  //   url: 'http://ctl.columbia.edu/tag/mediathread/feed/',
+  //   dataType: "jsonp text xml",
+  // });
+  
+  // Ok, I give up, do it for me Uncle Google
+  var xmlFeed = new google.feeds.Feed("http://ctl.columbia.edu/tag/mediathread/feed/");
+  xmlFeed.load(function(result){
+    if (!result.error) {
+      console.log(result.feed.entries.length);
+      // It works!^^^, now loop through the results and mark it up
+    }
+  });
+}
+
+$(document).ready(function(){
 	meth.bootstrap_drop_menu();
-})
+  meth.build_news_feed();
+});
 
 
