345ab6fdb4888ccf665058ee53cf8fd815267fbb
Nick Buonincontri <nbuonin@gmail.com>
2016-09-26 16:38:13 -0400

    javascript feed for mediathread news stories

--- static/js/custom.js
+++ static/js/custom.js
@@ -7,33 +7,41 @@ meth.bootstrap_drop_menu = function(){
 	  var clone_text = parent.clone().text();
 	  parent.remove();
 	  dropdown.prepend('<span>' + clone_text + '</span>');
-	})
-
+	});
 }
 
 meth.build_news_feed = function() {
-  // var xmlFeed;
+  var feedMarkup = "";
 
-  // var feedRequest = new XMLHttpRequest();
-  // feedRequest.open('GET', 'http://ctl.columbia.edu/tag/mediathread/feed/');
-  // // feedRequest.setRequestHeader("Authorization", "Negotiate");
-  // feedRequest.send();
-  // console.log(feedRequest.getAllResponseHeaders());
+  $.getJSON("http://ctl.columbia.edu/tag/mediathread/?feed=json&jsonp=?", function (data) {
+    console.log(data);
+    $.each(data, function (i, newsItem) {
+      // remove time from date string
+      newsItem.date = newsItem.date.replace(/\s\d{2}\:\d{2}/, "");
 
-  // this should work, but doesn't:
-  // var feedRequest = $.ajax({
-  //   method: 'GET',
-  //   url: 'http://ctl.columbia.edu/tag/mediathread/feed/',
-  //   dataType: "jsonp text xml",
-  // });
-  
-  // Ok, I give up, do it for me Uncle Google
-  var xmlFeed = new google.feeds.Feed("http://ctl.columbia.edu/tag/mediathread/feed/");
-  xmlFeed.load(function(result){
-    if (!result.error) {
-      console.log(result.feed.entries.length);
-      // It works!^^^, now loop through the results and mark it up
-    }
+      feedMarkup += "<div class='views-row'>";
+      feedMarkup += "  <div class='views-field views-field-title-1'>";
+      feedMarkup += "    <span class='field-content'>" + newsItem.title + "</span>";
+      feedMarkup += "  </div>";
+      feedMarkup += "  <div class='views-field views-field-url-1'>";
+      feedMarkup += "    <span class='field-content'>"
+      feedMarkup += "      <a href='" + newsItem.permalink + "'><h3>" + newsItem.title + "</h3></a>"
+      feedMarkup += "    </span>";
+      feedMarkup += "  </div>";
+      feedMarkup += "  <div class='views-field views-field-created'>";
+      feedMarkup += "    <span class='field-content'>" + newsItem.date + "</span>";
+      feedMarkup += "  </div>";
+      feedMarkup += "  <div class='views-field views-field-field-feed-item-description'>";
+      feedMarkup += "    <div class='field-content'>" + newsItem.excerpt + "</div>";
+      feedMarkup += "  </div>";
+      feedMarkup += "  <div class='views-field views-field-url'>";
+      feedMarkup += "    <span class='field-content read-more'>"
+      feedMarkup += "      <a href='" + newsItem.permalink + "'>Read More</a>"
+      feedMarkup += "    </span>";
+      feedMarkup += "  </div>";
+      feedMarkup += "</div>";
+    })
+    $(".view-id-mediathread_news .view-content").append(feedMarkup);
   });
 }
 
