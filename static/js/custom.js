17ac20387513ff1866d44b15fcb9232aff7dc63d
Nick Buonincontri <nbuonin@gmail.com>
2017-02-23 16:08:28 -0500

    Reformat spaces, use `` to build template.
    This commit replaces a series of string += to using ``. This makes it a
    bit easier to read.

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,52 +1,52 @@
 var meth = {};
 
 meth.bootstrap_drop_menu = function(){
-	jQuery('.dropdown').each(function(){
-	  var parent = jQuery(this).children('a').eq(0);
-	  var dropdown = jQuery(this).find('.dropdown-toggle');
-	  var clone_text = parent.clone().text();
-	  parent.remove();
-	  dropdown.prepend('<span>' + clone_text + '</span>');
-	});
+    jQuery('.dropdown').each(function(){
+        var parent = jQuery(this).children('a').eq(0);
+        var dropdown = jQuery(this).find('.dropdown-toggle');
+        var clone_text = parent.clone().text();
+        parent.remove();
+        dropdown.prepend('<span>' + clone_text + '</span>');
+    });
 }
 
 meth.build_news_feed = function() {
-  var feedMarkup = "";
+    var feedMarkup = "";
 
-  $.getJSON("http://ctl.columbia.edu/tag/mediathread/?feed=json&jsonp=?", function (data) {
-    $.each(data, function (i, newsItem) {
-      // remove time from date string
-      newsItem.date = newsItem.date.replace(/\s\d{2}\:\d{2}/, "");
+    $.getJSON("http://ctl.columbia.edu/tag/mediathread/?feed=json&jsonp=?", function (data) {
+        $.each(data, function (i, newsItem) {
+            // remove time from date string
+            newsItem.date = newsItem.date.replace(/\s\d{2}\:\d{2}/, "");
 
-      feedMarkup += "<div class='views-row'>";
-      feedMarkup += "  <div class='views-field views-field-title-1'>";
-      feedMarkup += "    <span class='field-content'>" + newsItem.title + "</span>";
-      feedMarkup += "  </div>";
-      feedMarkup += "  <div class='views-field views-field-url-1'>";
-      feedMarkup += "    <span class='field-content'>"
-      feedMarkup += "      <a href='" + newsItem.permalink + "'><h3>" + newsItem.title + "</h3></a>"
-      feedMarkup += "    </span>";
-      feedMarkup += "  </div>";
-      feedMarkup += "  <div class='views-field views-field-created'>";
-      feedMarkup += "    <span class='field-content'>" + newsItem.date + "</span>";
-      feedMarkup += "  </div>";
-      feedMarkup += "  <div class='views-field views-field-field-feed-item-description'>";
-      feedMarkup += "    <div class='field-content'>" + newsItem.excerpt + "</div>";
-      feedMarkup += "  </div>";
-      feedMarkup += "  <div class='views-field views-field-url'>";
-      feedMarkup += "    <span class='field-content read-more'>"
-      feedMarkup += "      <a href='" + newsItem.permalink + "'>Read More</a>"
-      feedMarkup += "    </span>";
-      feedMarkup += "  </div>";
-      feedMarkup += "</div>";
-    })
-    $(".view-id-mediathread_news .view-content").append(feedMarkup);
-  });
+            feedMarkup += `<div class='views-row'>
+              <div class='views-field views-field-title-1'>
+                <span class='field-content'>${newsItem.title}</span>
+              </div>
+              <div class='views-field views-field-url-1'>
+                <span class='field-content'>
+                  <a href='${newsItem.permalink}'><h3>${newsItem.title}</h3></a>
+                </span>
+              </div>
+              <div class='views-field views-field-created'>
+                <span class='field-content'>${newsItem.date}</span>
+              </div>
+              <div class='views-field views-field-field-feed-item-description'>
+                <div class='field-content'>${newsItem.excerpt}</div>
+              </div>
+              <div class='views-field views-field-url'>
+                <span class='field-content read-more'>
+                  <a href='${newsItem.permalink}'>Read More</a>
+                </span>
+              </div>
+            </div>`;
+        })
+        $(".view-id-mediathread_news .view-content").append(feedMarkup);
+    });
 }
 
 $(document).ready(function(){
-	meth.bootstrap_drop_menu();
-  meth.build_news_feed();
+    meth.bootstrap_drop_menu();
+    meth.build_news_feed();
 });
 
 
